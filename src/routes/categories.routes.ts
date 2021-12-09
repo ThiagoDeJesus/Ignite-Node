9c8a719c6f57c687f1cb0e1859e6032255bb64a4
ThiagoDeJesus <99thiagodejesus@gmail.com>
2021-12-09 20:45:31 -0300

    feat: refactor services to use cases and implements singleton

--- src/routes/categories.routes.ts
+++ src/routes/categories.routes.ts
@@ -1,25 +1,16 @@
-import { Router, Request, Response } from "express";
+import { Router } from "express";
 
-import { CategoriesRepository } from "../repositories/CategoriesRepository";
-import { CreateCategoryService } from "../services/CreateCategoryService";
+import { createCategoryController } from "../modules/cars/useCases/createCategory";
+import { listCategoriesController } from "../modules/cars/useCases/listCategories";
 
 const categoriesRoutes = Router();
-const categoriesRepository = new CategoriesRepository();
 
-categoriesRoutes.post("/", (req: Request, res: Response) => {
-  const { name, description } = req.body;
+categoriesRoutes.post("/", (req, res) =>
+  createCategoryController.handle(req, res)
+);
 
-  const createCategoryService = new CreateCategoryService(categoriesRepository);
-
-  createCategoryService.execute({ name, description });
-
-  return res.status(201).send();
-});
-
-categoriesRoutes.get("/", (req: Request, res: Response) => {
-  const all = categoriesRepository.list();
-
-  return res.json(all);
-});
+categoriesRoutes.get("/", (req, res) =>
+  listCategoriesController.handle(req, res)
+);
 
 export { categoriesRoutes };
