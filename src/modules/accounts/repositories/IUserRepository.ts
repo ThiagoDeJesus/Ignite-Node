231ac50788cdecb57b274eb69c4b6b82b6a1ffc4
ThiagoDeJesus <99thiagodejesus@gmail.com>
2021-12-13 20:51:03 -0300

    feat: encrypt password and add findByEmail

--- src/modules/accounts/repositories/IUserRepository.ts
+++ src/modules/accounts/repositories/IUserRepository.ts
@@ -1,7 +1,9 @@
 import { ICreateUserDto } from "../dtos/ICreateUserDTO";
+import { User } from "../entities/User";
 
 interface IUsersRepository {
   create(data: ICreateUserDto): Promise<void>;
+  findByEmail(email: string): Promise<User>;
 }
 
 export { IUsersRepository };
